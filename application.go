42dcfb399014fa047b17df01facdea08f062210c
Owen <owendearmond@gmail.com>
2023-01-19 21:10:25 -0800

    Add dependencies, credential reference

--- application.go
+++ application.go
@@ -1,16 +1,47 @@
 package main
 
+// > go mod init pluginpricing
+// > go get github.com/aws/aws-sdk-go-v2
+// > go get github.com/aws/aws-sdk-go-v2/config
+// > go get github.com/aws/aws-sdk-go-v2/service/elasticbeanstalk
+// > go get github.com/aws/aws-sdk-go-v2/service/rds
+
 import (
     "io/ioutil"
     "log"
     "net/http"
     "os"
+    "context"
+    "database/sql"
+    "fmt"
+
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go-v2/feature/rds/auth"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+
+    var dbName string "database-1-instance-1"
+    var dbUser string "admin"
+    var dbHost string = "database-1-instance-1.cl7yuwuzzgbr.us-west-2.rds.amazonaws.com"
+    var dbPort int = 3306
+    var dbEndpoint string = fmt.Sprintf("%s:%d", dbHost, dbPort)
+    var region string = "us-west-2"
+
+    sess := session.Must(session.NewSession())
+    creds := sess.Config.Credentials
+
     port := os.Getenv("PORT")
     if port == "" {
 	port = "5000"
+    }
+
+    authenticationToken, err := auth.BuildAuthToken(
+    	context.TODO(), dbEndpoint, region, dbUser, cfg.Credentials)
+    if err != nil {
+	    panic("failed to create authentication token: " + err.Error())
     }
 
     f, _ := os.Create("/var/log/golang/golang-server.log")
